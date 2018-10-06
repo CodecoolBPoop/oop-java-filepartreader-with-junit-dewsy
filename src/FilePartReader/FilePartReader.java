c70dde06ac8b339e5de3e9946c87f17a657006a9
dewsy <dewsyy@gmail.com>
2018-10-06 15:10:49 +0200

    filereader started

--- src/FilePartReader/FilePartReader.java
+++ src/FilePartReader/FilePartReader.java
@@ -1,4 +1,21 @@
 package FilePartReader;
 
+
 public class FilePartReader {
+
+    private String filePath;
+    private int fromLine;
+    private int toLine;
+
+
+    public FilePartReader() {
+        this.filePath = "SFSDVQsdv";
+        this.fromLine = -2;
+        this.toLine = -3;
+    }
+
+    public void setuo(){
+        if (this.fromLine > this.toLine) throw new IllegalArgumentException("finish must come later than start");
+        if (this.fromLine < 1) throw new IllegalArgumentException("gotta start at least @ line 1");
+    }
 }
